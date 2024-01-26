3b527d2a3d444708295d02a992bbe11a6f446274
Marina Novikova <Marina-Novikova@gmail.com>
2024-01-26 11:54:47 +0300

    add a HomeWork HW-3

---
+++ HW3/1/HW3-1/HW3-1.cpp
@@ -0,0 +1,97 @@
+﻿// HW3-1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+class Calculator
+{
+private:
+	double num1;
+	double num2;
+public:
+	double add()
+	{
+		return num1 + num2;
+	}
+	double multiply()
+	{
+		return num1 * num2;
+	}
+	double subtract_1_2()
+	{
+		return num1 - num2;
+	}
+	double subtract_2_1()
+	{
+		return num2 - num1;
+	}
+	double divide_1_2()
+	{
+		return num1 / num2;
+	}
+	double divide_2_1()
+	{
+		return num2 / num1;
+	}
+	//метод должен установить значение поля num1 равное значению аргумента num1 в случае, если значение аргумента не равно 0. 
+	// Возвращает true, если установка произошла успешно, в обратном случае false
+	bool set_num1(double num1)
+	{
+		if (num1 == 0) return false;
+		this->num1 = num1;
+		return true;
+	}
+	bool set_num2(double num2)
+	{
+		if (num2 == 0) return false;
+		this->num2 = num2;
+		return true;
+	}
+
+};
+#include <iostream>
+
+int main()
+{
+	double num1;
+	double num2;
+	bool res;
+
+	Calculator myCalculator;
+	
+	setlocale(LC_ALL, "Russian");
+	while (true)
+	{
+		do
+		{
+			//--
+			std::cout << "Введите num1: ";
+			std::cin >> num1;
+			res = myCalculator.set_num1(num1);
+			if (!res)
+			{
+				std::cout << "Неверный ввод!\n";
+			}
+		} while (!res);
+		do
+		{
+			//--
+			std::cout << "Введите num2: ";
+			std::cin >> num2;
+			res = myCalculator.set_num2(num2);
+			if (!res)
+			{
+				std::cout << "Неверный ввод!\n";
+			}
+		} while (!res);
+		//
+		std::cout << "num1 + num2 = " << myCalculator.add() << std::endl;
+		std::cout << "num1 - num2 = " << myCalculator.subtract_1_2() << std::endl;
+		std::cout << "num2 - num1 = " << myCalculator.subtract_2_1() << std::endl;
+		std::cout << "num1 * num2 = " << myCalculator.multiply() << std::endl;
+		std::cout << "num1 / num2 = " << myCalculator.divide_1_2() << std::endl;
+		std::cout << "num2 / num1 = " << myCalculator.divide_2_1() << std::endl;
+
+
+	}//while true
+}//int main
+
+
+
---
+++ HW3/2/HW3-2/HW3-2.cpp
@@ -0,0 +1,111 @@
+﻿// HW3-2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+class Counter
+{
+private:
+    int count;
+public:
+    int getCount ()
+    {
+        return (count);
+    }
+    void increment()
+    {
+        count++;
+    }
+    void decrement()
+    {
+        count--;
+    }
+    Counter ()
+    {
+        count = 0;
+    }
+    Counter (int count)
+    {
+        this->count = count;
+    }
+};
+
+int main()
+{
+    std::string yesOrNo;
+    int count;
+    std::string cmd;
+
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите yes или no: ";
+    std::cin >> yesOrNo;
+    if (yesOrNo == "yes")
+    {
+
+        std::cout << "Введите начальное значение счётчика: ";
+        std::cin >> count;
+        
+        Counter myCounter(count);
+
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> cmd;
+        while (cmd != "x")
+        {
+            if (cmd == "+")
+            {
+                myCounter.increment();
+            }
+            else if (cmd == "-")
+            {
+                myCounter.decrement();
+            }
+            else if (cmd == "=")
+            {
+                std::cout << myCounter.getCount() << std::endl;
+            }
+            else
+            {
+                //nothing
+            }
+            std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+            std::cin >> cmd;
+        }//while cmd
+        std::cout << "До свидания!";
+
+    }//yes
+    else
+    {
+
+        Counter myCounter;
+
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> cmd;
+        while (cmd != "x")
+        {
+            if (cmd == "+")
+            {
+                myCounter.increment();
+            }
+            else if (cmd == "-")
+            {
+                myCounter.decrement();
+            }
+            else if (cmd == "=")
+            {
+                std::cout << myCounter.getCount() << std::endl;
+            }
+            else
+            {
+                //nothing
+            }
+            std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+            std::cin >> cmd;
+        }//while cmd
+        std::cout << "До свидания!";
+
+    }//no
+
+}//int main()
+
+
