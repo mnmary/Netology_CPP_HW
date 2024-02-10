28612141ed3b97b053f9c265f76174beb8a0b4b7
Marina Novikova <Marina-Novikova@gmail.com>
2024-02-10 18:58:40 +0300

    add Homework HW7

---
+++ HW7/1/HW7-1/HW7-1.cpp
@@ -0,0 +1,37 @@
+﻿// HW7-1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+#define MODE 1
+
+#ifndef MODE
+#error "Необходимо сформировать переменную MODE"
+#endif
+
+#if MODE == 1
+int add(int a, int b)
+{
+    return a + b;
+}
+#endif
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+#if MODE==0
+    std::cout << "Работаю в режиме тренировки";
+#elif MODE == 1
+    int a, b;
+    std::cout << "Работаю в боевом режиме" << std::endl;
+    std::cout << "Введите число 1: ";
+    std::cin >> a;
+    std::cout << "Введите число 2: ";
+    std::cin >> b;
+    std::cout << "Результат сложения: " << add(a,b);
+
+#else
+    std:: cout << "Неизвестный режим. Завершение работы";
+#endif
+}
+
---
+++ HW7/2/HW7-2/HW7-2.cpp
@@ -0,0 +1,16 @@
+﻿// HW7-2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+#define SUB(a, b) (a) - (b)
+
+int main()
+{
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << (SUB(a, b)) * c << std::endl;
+	std::cout << (SUB(a, b + c)) * c << std::endl;
+}
