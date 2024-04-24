d8d8ca3ec728ef754ecc41edc138e2e1b81d3235
Marina Novikova <Marina-Novikova@gmail.com>
2024-04-24 10:22:56 +0300

    add CPPL HW2

---
+++ CPPL HW2/HW2-1/HW2-1.cpp
@@ -0,0 +1,31 @@
+﻿// HW2-1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+struct point {
+	double m_x;
+	double m_y;
+	point(double x, double y) {
+		m_x = x;
+		m_y = y;
+	}
+};
+
+void print_point(const point& point_object) {
+	std::cout << "x:" << point_object.m_x << ", y: "
+		<< point_object.m_y << std::endl;
+}
+
+int main()
+{
+	int i;
+	for (i = 0; i < 5; i++) {
+		point my_point(i, 2 * i);
+		print_point(my_point);
+	}
+	return 0;
+}
+
+
