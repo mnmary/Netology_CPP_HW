f5954f9fd820bbc57ab43529a2eeb8384b2fc2e3
Marina Novikova <Marina-Novikova@gmail.com>
2024-06-24 16:46:46 +0300

    delete tmp files

--- SQLCPP HW05/out/build/x64-Debug/libpqxx-build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/libpqxx-7.9.1/test
-# Build directory: F:/CPP Homeworks/SQLCPP HW05/out/build/x64-Debug/libpqxx-build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(runner "F:/CPP Homeworks/SQLCPP HW05/out/build/x64-Debug/libpqxx-build/test/runner.exe")
-set_tests_properties(runner PROPERTIES  _BACKTRACE_TRIPLES "F:/libpqxx-7.9.1/test/CMakeLists.txt;12;add_test;F:/libpqxx-7.9.1/test/CMakeLists.txt;0;")
