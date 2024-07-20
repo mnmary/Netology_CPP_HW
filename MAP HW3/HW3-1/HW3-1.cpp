ecbaa0617ee4dde19cb111f329fe450684da5da6
Marina Novikova <Marina-Novikova@gmail.com>
2024-07-20 17:21:48 +0300

    add CPP MAP HW3

---
+++ MAP HW3/HW3-1/HW3-1.cpp
@@ -0,0 +1,60 @@
+﻿// HW3-1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <thread>
+#include <future>
+void getMin(std::vector<int> v, int i, std::promise<int> pr)
+{
+	size_t n = v.size();
+
+	int minV{ i };
+	for (int j = i + 1; j < n; j++)
+	{
+		if (v[j] < v[minV])
+		{
+			minV = j;
+		}
+	}
+	pr.set_value(minV);
+};
+void print_vector(std::vector<int>& v)
+{
+	for (auto& v1 : v)
+	{
+		std::cout << v1 << " ";
+	}
+	std::cout << std::endl;
+}
+int main()
+{
+	std::vector<int> v{ 9,1,5,3,2,4 };
+	std::cout << "Before: ";
+	print_vector(v);
+
+	auto n = v.size();
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		//1. promise
+		std::promise<int> pr;
+		//2. future
+		std::future<int> fut = pr.get_future();
+		//3. stream
+		auto res = async(getMin, v, i, move(pr));
+		//4. wait promise.set_value
+		int minIndex = fut.get();
+
+		//5. sort
+		if (v[minIndex] != v[i])
+		{
+			std::swap(v[i], v[minIndex]);
+		}
+
+	}
+	std::cout << "After: ";
+	print_vector(v);
+
+	return 0;
+}
+
---
+++ MAP HW3/HW3-2/HW3-2.cpp
@@ -0,0 +1,63 @@
+﻿// HW3-2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <future>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+void add(int& val)
+{
+	val++;
+}
+
+template<typename Iterator>
+void parallel_for_each(Iterator first, Iterator last)
+{
+	//vector len
+	auto length = std::distance(first, last);
+	//divider
+	unsigned long const max_chunk_size = 2;
+	if (length <= max_chunk_size)
+	{
+		//increment
+		std::for_each(first, last, add);
+		//nothing
+	}
+	else
+	{
+		//divide
+		Iterator mid_point = first;
+		std::advance(mid_point, length / 2);
+		//future
+		auto res = std::async(parallel_for_each<Iterator>, first, mid_point);
+		//recursive
+		parallel_for_each(mid_point, last);
+		//wait
+		res.get();
+	}
+}
+
+void print_vector(std::vector<int>& v)
+{
+	for (auto v1 : v)
+	{
+		std::cout << v1 << " ";
+	}
+	std::cout << std::endl;
+}
+
+int main()
+{
+	std::vector<int> v {9,1,5,6,4,3};
+	std::cout << "Before: ";
+	print_vector(v);
+
+	parallel_for_each(v.begin(), v.end());
+
+	std::cout << "After: ";
+	print_vector(v);
+
+	return 0;
+}
