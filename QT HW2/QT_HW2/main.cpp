4f07792bc15ff6e3bce4050fa14e2e4146a52eb9
Marina Novikova <Marina-Novikova@gmail.com>
2024-09-11 21:13:11 +0300

    add QT HW2

---
+++ QT HW2/QT_HW2/main.cpp
@@ -0,0 +1,13 @@
+#include <QCoreApplication>
+#include <QUdpSocket>
+#include <QTcpSocket>
+#include <QSqlDatabase>
+
+int main(int argc, char *argv[])
+{
+    QSqlDatabase DB;
+    QUdpSocket UDPSocket;
+    QTcpSocket TCPSocket;
+
+    return 0;
+}
